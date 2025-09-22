af4abb906b4bbe00f0a233534b5092726279ddde
BAESUBEEN <bsbi1219@naver.com>
2025-09-22 12:59:40 +0900

    캐릭터의 위치 수정 350->380

--- animation_viewer.py
+++ animation_viewer.py
@@ -13,19 +13,19 @@ while True:
     for x in range(0, 5):
         for frame in range(0, 6): # attack 1
             clear_canvas()
-            char_attack.clip_draw(frame * 128, 0, 128, 128, 400, 350, 600, 600)
+            char_attack.clip_draw(frame * 128, 0, 128, 128, 400, 380, 600, 600)
             update_canvas()
             delay(0.1)
 
         for frame in range(0, 4):
             clear_canvas()
-            char_attack2.clip_draw(frame * 128, 0, 128, 128, 400, 350, 600, 600)
+            char_attack2.clip_draw(frame * 128, 0, 128, 128, 400, 380, 600, 600)
             update_canvas()
             delay(0.1)
 
         for frame in range(0, 3):
             clear_canvas()
-            char_attack3.clip_draw(frame * 128, 0, 128, 128, 400, 350, 600, 600)
+            char_attack3.clip_draw(frame * 128, 0, 128, 128, 400, 380, 600, 600)
             update_canvas()
             delay(0.1)
 
@@ -34,7 +34,7 @@ while True:
     for x in range(0, 5):
         for frame in range(0, 8):
             clear_canvas()
-            char_run.clip_draw(frame * 128, 0, 128, 128, 400, 350, 600, 600)
+            char_run.clip_draw(frame * 128, 0, 128, 128, 400, 380, 600, 600)
             update_canvas()
             delay(0.1)
 
@@ -43,7 +43,7 @@ while True:
     for x in range(0, 5):
         for frame in range(0, 6):  # attack 1
             clear_canvas()
-            char_dead.clip_draw(frame * 128, 0, 128, 128, 400, 350, 600, 600)
+            char_dead.clip_draw(frame * 128, 0, 128, 128, 400, 380, 600, 600)
             update_canvas()
             delay(0.15)
 
@@ -52,7 +52,7 @@ while True:
     for x in range(0, 5):
         for frame in range(0, 15):  # attack 1
             clear_canvas()
-            char_jump.clip_draw(frame * 128, 0, 128, 128, 400, 350, 600, 600)
+            char_jump.clip_draw(frame * 128, 0, 128, 128, 400, 380, 600, 600)
             update_canvas()
             delay(0.1)
 
