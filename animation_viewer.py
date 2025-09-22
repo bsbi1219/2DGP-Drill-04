04cfaea2eb5c31952bb7cd1a4fb5200d43654d05
BAESUBEEN <bsbi1219@naver.com>
2025-09-22 12:50:09 +0900

    캐릭터의 run기능 구현 에니메이션 5회 반복 재생 구현 각 에니메이션 사이에 1초의 딜레이 시간 추가

--- animation_viewer.py
+++ animation_viewer.py
@@ -10,23 +10,33 @@ char_dead = load_image('Dead.png')
 char_jump = load_image('Jump.png')
 
 while True:
-    for frame in range(0, 6): # attack 1
-        clear_canvas()
-        char_attack.clip_draw(frame * 128, 0, 128, 128, 400, 300, 400, 400)
-        update_canvas()
-        delay(0.1)
+    for x in range(0, 5):
+        for frame in range(0, 6): # attack 1
+            clear_canvas()
+            char_attack.clip_draw(frame * 128, 0, 128, 128, 400, 350, 600, 600)
+            update_canvas()
+            delay(0.1)
 
-    for frame in range(0, 4):
-        clear_canvas()
-        char_attack2.clip_draw(frame * 128, 0, 128, 128, 400, 300, 400, 400)
-        update_canvas()
-        delay(0.1)
+        for frame in range(0, 4):
+            clear_canvas()
+            char_attack2.clip_draw(frame * 128, 0, 128, 128, 400, 350, 600, 600)
+            update_canvas()
+            delay(0.1)
 
-    for frame in range(0, 3):
-        clear_canvas()
-        char_attack3.clip_draw(frame * 128, 0, 128, 128, 400, 300, 400, 400)
-        update_canvas()
-        delay(0.1)
+        for frame in range(0, 3):
+            clear_canvas()
+            char_attack3.clip_draw(frame * 128, 0, 128, 128, 400, 350, 600, 600)
+            update_canvas()
+            delay(0.1)
+
+    delay(1)
+
+    for x in range(0, 5):
+        for frame in range(0, 8):
+            clear_canvas()
+            char_run.clip_draw(frame * 128, 0, 128, 128, 400, 350, 600, 600)
+            update_canvas()
+            delay(0.1)
 
 
 
